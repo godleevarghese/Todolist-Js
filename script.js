c87e4e0bf4c3eb33a3e9d788a74c62580a91589c
godleevarghese <godleevarghese@gmail.com>
2021-01-08 16:03:19 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+const inputBox = document.querySelector(".inputField input")
+const addBtn = document.querySelector(".inputField button")
+const todoList = document.querySelector(".todoList")
+const deleteAllBtn = document.querySelector(".footer button")
+
+
+
+inputBox.onkeyup = ()=>{
+    let userData = inputBox.value
+    if(userData.trim() != 0 ){
+        addBtn.classList.add("active")
+    }
+    else{
+        addBtn.classList.remove("active")
+    }
+}
+
+showTask()
+
+
+addBtn.onclick = ()=>{
+    let userData = inputBox.value
+    let getLocalStorage = localStorage.getItem("New Todo")
+    if(getLocalStorage==null){
+        listArr = []
+    }else{
+        listArr = JSON.parse(getLocalStorage)
+    }
+    listArr.push(userData)
+    localStorage.setItem("New Todo",JSON.stringify(listArr))
+    showTask()
+}
+
+function showTask(){
+
+    let getLocalStorage = localStorage.getItem("New Todo")
+    if(getLocalStorage==null){
+        listArr = []
+    }else{
+        listArr = JSON.parse(getLocalStorage)
+    }
+    const pendingNum = document.querySelector(".pendingNum")
+    pendingNum.textContent = listArr.length
+    if(listArr.length>0){
+        deleteAllBtn.classList.add("active")
+    }else{
+        deleteAllBtn.classList.remove("active")
+    }
+
+    let newlitag = ''
+    listArr.forEach((element,index) => {
+        newlitag += `<li> ${element}<span onclick ="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`
+    });
+    todoList.innerHTML=newlitag
+    inputBox.value=""
+}
+
+function deleteTask(index){
+    let getLocalStorage = localStorage.getItem("New Todo")
+    listArr = JSON.parse(getLocalStorage)
+    listArr.splice(index,1)
+    localStorage.setItem("New Todo",JSON.stringify(listArr))
+    showTask()
+}
+
+deleteAllBtn.onclick = ()=>{
+
+    listArr = []
+    localStorage.setItem("New Todo",JSON.stringify(listArr))
+    showTask()
+}
